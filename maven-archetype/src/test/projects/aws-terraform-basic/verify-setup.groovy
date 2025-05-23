0d08949cf67616fdc666b36a9e77ae0502a37cd6
Anton Epple <toni.epple@smart-access-solutions.com>
2025-05-23 14:11:02 +0200

    refs #20 fix profile in groovy scripts

--- maven-archetype/src/test/projects/aws-terraform-basic/verify-setup.groovy
+++ maven-archetype/src/test/projects/aws-terraform-basic/verify-setup.groovy
@@ -1,6 +1,6 @@
 println ">> Running Terraform apply for testing"
 def dir = new File(basedir, "aws-demo")
-def process = ["mvn", "package", "-Pterraform", "-Dterraform.action=apply"].execute(null, dir)
+def process = ["mvn", "package", "-Pdeploy-with-terraform", "-Dterraform.action=apply"].execute(null, dir)
 process.in.eachLine { println it }
 int result = process.waitFor()
 if (result != 0) {
